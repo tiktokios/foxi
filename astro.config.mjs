49432abc6440a0a2758d2c14bae356049086807c
tiktokios <tiktokiocdn@gmail.com>
2024-07-23 20:18:54 +0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,8 @@ import { defineConfig } from "astro/config";
 import partytown from "@astrojs/partytown";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+
 import sitemap from "@astrojs/sitemap";
-import astroI18next from "astro-i18next";
-
-
 
 // https://astro.build/config
 export default defineConfig({
